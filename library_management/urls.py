4edf7568242afe0d9fd6d844065a4c41f3541aaf
Lohar Mayank T. <loharmayank701@gmail.com>
2025-04-05 13:44:49 +0530

    updated code with qrcode

--- library_management/urls.py
+++ library_management/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('', include('library.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
